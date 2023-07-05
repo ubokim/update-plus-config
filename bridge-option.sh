dcfaee3325afeefe8f3cc469ca29308c69923e77
ubokim <138520427+ubokim@users.noreply.github.com>
2023-07-05 16:49:36 -0400

    fixed EOL syntax

--- bridge-option.sh
+++ bridge-option.sh
@@ -31,33 +31,25 @@ read -p "Listen Port : " ServerTransportListenAddr
 # Configure inputs into config file 
 printf "\n${blue}[~] Configuring Bridge file ...${clear}\n"
 cat > /etc/tor/torrc << EOL
-
 BridgeRelay 1
-
 # Replace "TODO1" with a Tor port of your choice.
 # This port must be externally reachable.
 # Avoid port 9001 because it's commonly associated with Tor and censors may be scanning the Internet for this port.
 ORPort ${ORPort}
-
 ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy
-
 # Replace "TODO2" with an obfs4 port of your choice.
 # This port must be externally reachable and must be different from the one specified for ORPort.
 # Avoid port 9001 because it's commonly associated with Tor and censors may be scanning the Internet for this port.
 ServerTransportListenAddr obfs4 0.0.0.0:${ServerTransportListenAddr}
-
 # Local communication port between Tor and obfs4.  Always set this to "auto".
 # "Ext" means "extended", not "external".  Don't try to set a specific port number, nor listen on 0.0.0.0.
 ExtORPort auto
-
 # Replace "<address@email.com>" with your email address so we can contact you if there are problems with your bridge.
 # This is optional but encouraged.
 ContactInfo <${ContactInfo}> @ator: ${Wallet}
-
 # Pick a nickname that you like for your bridge.  This is optional.
 Nickname ${Nickname}
-
-EOL   
+EOL
 
 # enable and restart the service
 printf "\n${purple}[~] Enabling & Starting the Service ...${clear}\n"
